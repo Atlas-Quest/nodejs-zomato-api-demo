efa27d1f998c710fb6afa6cbde49ca06b74ba136
atlas-quest <vanessa@sveltestudios.com>
2020-01-24 15:50:56 +1030

    details on page

--- components/RestaurantDetails.vue
+++ components/RestaurantDetails.vue
@@ -1,6 +1,43 @@
 <template>
-<div v-if="active">
-  {{restaurant.name}}
+<div v-if="active"
+     class="flex p-12">
+  <div class="flex justify-start">
+    <img :src="restaurant.thumb"
+         alt="">
+  </div>
+  <div class="flex flex-col">
+    <div class="flex">
+      {{restaurant.name}}
+    </div>
+    <div class="flex">
+      {{restaurant.location.address}}
+    </div>
+    <div v-if="restaurant.has_table_booking"
+         class="flex">
+      tick
+    </div>
+    <div v-else
+         class="flex">
+      cross
+    </div>
+    <div v-if="restaurant.is_delivering_now"
+         class="flex">
+      tick
+    </div>
+    <div v-else
+         class="flex">
+      cross
+    </div>
+    <div class="">
+      {{restaurant.cuisines}}
+    </div>
+    <div class="">
+      {{restaurant.phone_numbers}}
+    </div>
+    <div class="">
+      {{restaurant.timings}}
+    </div>
+  </div>
 </div>
 </template>
 
